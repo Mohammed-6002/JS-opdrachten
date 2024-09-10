c3d4a529f76c1f4d1c8d834d9de4d2aaafb6843d
Mohammed-6002 <302352550@student.rocmondriaan.nl>
2024-09-10 10:17:55 +0200

    Sprint 1

---
+++ Sprint 1/main.js
@@ -0,0 +1,16 @@
+//Opdrachten variabelen, H 2.1
+const myName = 'Mohammed';
+let currentTemp;
+currentTemp = 20;
+console.log(myName);
+alert(`Hello ${myName}, het is ${currentTemp} graden buiten`);
+currentTemp += 5;
+console.log(currentTemp);
+//Opdrachten interactie, H 2.2
+const userName = "Mo";
+prompt('Wat is je naam')
+alert('Hallo' + userName);
+const toestemming = confirm('Mag ik cookies opslaan');
+console.log('Toesteming voor cookies:' + toestemming);
+const myCity = 'Den Haag';
+alert('Ik woon in' + myCity)
